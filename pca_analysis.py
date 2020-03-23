b103cc9932914a260c74581173a11813674a1dbd
Danusorn Sitdhirasdr <41460732+Nak007@users.noreply.github.com>
2020-03-23 22:29:27 +0700

    Create pca_analysis.py

---
+++ pca_analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd, numpy as np, os, math
+import matplotlib.pylab as plt
+from matplotlib import cm
+from scipy import stats
+from scipy.stats import gaussian_kde
+from factor_analyzer import FactorAnalyzer
